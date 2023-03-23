f838495c6e13efb9b64f0f9f2b579fca6da6f2e6
LEEHYUNDONG <hd.lee1@cj.net>
2023-03-23 16:34:05 +0900

    feat :[230323][이현동] 전략패턴 적용 before

--- src/main/java/com/hyundong/designpattern/strategy/before/Cat.java
+++ src/main/java/com/hyundong/designpattern/strategy/before/Cat.java
@@ -2,10 +2,14 @@ package com.hyundong.designpattern.strategy.before;
 
 public class Cat {
     void sound(){
-        System.out.println("[Cat] 야옹");
+        System.out.println("고양이가 울다.");
     }
 
     void roaming(){
-        System.out.println("[Cat] 사뿐사뿐");
+        System.out.println("고양이가 사뿐사뿐 걷습니다.");
+    }
+
+    void breed(){
+        System.out.println("새끼를 낳았습니다");
     }
 }
--- src/main/java/com/hyundong/designpattern/strategy/before/Dog.java
+++ src/main/java/com/hyundong/designpattern/strategy/before/Dog.java
@@ -3,10 +3,14 @@ package com.hyundong.designpattern.strategy.before;
 public class Dog {
 
     void sound(){
-        System.out.println("[Dog] 왕왕");
+        System.out.println("강아지가 짖습니다.");
     }
 
     void roaming(){
-        System.out.println("[Dog] 총총");
+        System.out.println("강아지가 걷는다.");
+    }
+
+    void breed(){
+        System.out.println("새끼를 낳았습니다.");
     }
 }
